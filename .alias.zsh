c86f474b898e05696fce37ea512dd4cbbabdd237
cyio <icaner@qq.com>
2017-04-14 18:24:08 +0800

    update

--- .alias.zsh
+++ .alias.zsh
@@ -11,7 +11,6 @@ alias goo='proxychains googler -l zh --colors bjdxxy'
 # alias vim="nvim"
 # mosh
 alias spc='mosh oaker@192.168.1.143'
-alias scb='mosh oaker@192.168.1.115'
 alias smb='mosh --ssh="ssh -p 8022" u0_a78@192.168.1.233'
 function gi() { curl -L -ttps://www.gitignore.io/api/$@ ;}
 function lazygit() {
